8d72e852a99bf729e4704fc763f0f26240afeca6
Arkinion <nicholasgr44@gmail.com>
2018-07-16 17:48:07 -0400

    Added items

---
+++ src/main/java/com/arkinion/momod/items/ItemBase.java
@@ -0,0 +1,26 @@
+package com.arkinion.momod.items;
+
+import com.arkinion.momod.Main;
+import com.arkinion.momod.init.ModItems;
+import com.arkinion.momod.util.IHasModel;
+
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
+
+public class ItemBase extends Item implements IHasModel
+{
+	public ItemBase(String name)
+	{
+		setUnlocalizedName(name);
+		setRegistryName(name);
+		setCreativeTab(CreativeTabs.MATERIALS);
+		
+		ModItems.ITEMS.add(this);
+	}
+	
+	@Override
+	public void registerModels()
+	{
+		Main.proxy.registerItemRenderer(this, 0, "inventory");
+	}	
+}
---
+++ src/main/java/com/arkinion/momod/util/IHasModel.java
@@ -0,0 +1,6 @@
+package com.arkinion.momod.util;
+
+public interface IHasModel
+{
+	public void registerModels();
+}
